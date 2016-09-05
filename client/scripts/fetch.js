a6e8dc23f1fbf5dbdb66f65e4dc742318f9502dd
Taylor Page <jonathantaylorpage@gmail.com>
2016-09-05 12:40:29 -0500

    (feat) Allow DOM to be updated after data recieved

--- client/scripts/fetch.js
+++ client/scripts/fetch.js
@@ -3,13 +3,27 @@ function gitFetch(user, repo, callback) {
   fetch(url).then(function(response) {
     return response.json();
   }).then(function(data) {
-    callback(data);
+    callback(data, repo);
   })
 }
 
+function domManipulator(data, repo) {
+  document.getElementById(`${repo}-stars`).innerHTML = data.stargazers_count;
+  document.getElementById(`${repo}-forks`).innerHTML = data.forks_count;
+  document.getElementById(`${repo}-issues`).innerHTML = data.open_issues_count;
+  console.log(data)
+}
+
+(function updateDom() {
+  gitFetch('facebook', 'react', domManipulator);
+  gitFetch('angular', 'angular.js', domManipulator);
+  gitFetch('emberjs', 'ember.js', domManipulator);
+  gitFetch('vuejs', 'vue', domManipulator);
+})();
+
 
 /*
-*  Alternatively workers can be used to update information without interupting the user interface processes. This does not work in Chrome *  or Firefox when the origin is Localhost.
+  Alternatively web workers can be used to update information without interupting the user interface processes. This does not work in Chrome or Firefox when the origin is Localhost.
 */
 
 // function workerFetch(user, repo) {
